fadb9c1b34d4f555965501fdf8d7eefb51fcf689
mycommerce <phelelanigabela@gmail.com>
2023-08-25 11:47:13 +0200

    Siphesihle added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Welcome to our TYD</div>
+    <div>Welcome to our TYD
+       <h2>Hi it me Siphesihle</h2>
+    </div>
+   
+
   )
 }
 
