b56b80e47607f94c6b9d749648b82773eb3112d7
Darklove12 <bsinukela31@gmail.com>
2023-08-25 15:26:02 +0200

    tailwindcss and Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar'
 
 const App = () => {
   return (
-    <div>Welcome to our TYD
-      <h1> Hello Melusi</h1>
-      <h1>Hi it me Siphesihle</h1>
-      <h1>Hi it is me Fortunate</h1>
-      <h1>Hi it is me Mbuyiselo</h1>
+    <div>
+      <Navbar/>
     </div>
 
   )
