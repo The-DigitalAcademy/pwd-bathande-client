faa2ed14db84ce48c75568998ed69567391648e6
mycommerce <phelelanigabela@gmail.com>
2023-08-25 13:11:57 +0200

    we do want coding added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
       <h1>Hi it me Siphesihle</h1>
       <h1>Hi it is me Fortunate</h1>
       <h1>Hi it is me Mbuyiselo</h1>
+      <h1>Do you want love the coding</h1>
     </div>
     
   )
