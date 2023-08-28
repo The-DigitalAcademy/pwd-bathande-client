e88920184d57f39e228e894c27f9126f1c9da44a
Mbuyiselo91 <mbuyisolo@gmail.com>
2023-08-28 14:08:24 +0200

    Controller and routes,products,data

---
+++ server/routes/productRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const {getAllproducts} = require('../controllers/productCtrl')
+
+router.get('/', getAllproducts)
+
+module.exports = router
