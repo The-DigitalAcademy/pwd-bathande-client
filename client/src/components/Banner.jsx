6316c383d4bb3e50d80bc870ce811cb507bca2ab
Darklove12 <bsinukela31@gmail.com>
2023-08-28 16:21:05 +0200

    added imprt React

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+// import '../banner/Banner.css'
+
+// const Banner = () => {
+//   return (
+//     <div>
+//     <div className="container">
+//       <br /><br /><br /><br />
+//       <h1 className="market">Welcome to Neighborhood Market</h1>
+//     </div>
+
+//     <div className="card">
+//       <div className="card card-compact w-96 bg-base-100 shadow-xl">
+//         <figure>
+//           <img
+//             src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+//             alt="Shoes"
+//           />
+//         </figure>
+        
+//       </div>
+//     </div>
+//   </div>
+//   )
+// }
+
+// export default Banner
