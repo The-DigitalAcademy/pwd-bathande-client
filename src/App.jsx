a07038e6b457f02cce72910cf9f0195a2b1dc65f
Darklove12 <bsinukela31@gmail.com>
2023-08-28 11:57:37 +0200

    src and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Header from './components/navbar/Header'
+import Front from './components/Front'
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <Front/>
     </div>
 
   )           
---
+++ src/components/Front.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Front() {
+  return (
+    <div>
+
+<div className="carousel h-3 rounded-box">
+  <div className="carousel-item ">
+    <img src="https://images.unsplash.com/photo-1591085686350-798c0f9faa7f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80" alt="Burger" />
+  </div> 
+</div>
+    </div>
+  )
+}
+
+export default Front
