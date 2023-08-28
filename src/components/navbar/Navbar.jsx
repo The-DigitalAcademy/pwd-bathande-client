cff3fb884786e4e24b2bcb26ce6eba47c83151ab
Tshembani30 <thembaniasasent982@gmail.com>
2023-08-28 09:58:00 +0200

    search bar added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -45,38 +45,18 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          <a className="btn btn-ghost normal-case text-xl">Welcome to Neighborhood Market</a>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
+        <div className="form-control">
+      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+    </div>
 
-        <div className="dropdown dropdown-end">
+        <div className="dropdown dropdown-end ml-80">
           <label tabIndex={0} className="btn btn-ghost btn-circle">
             <div className="indicator">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
+                className="h-5 w-9 "
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -88,14 +68,14 @@ const Navbar = () => {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <span className="badge badge-sm indicator-item">8</span>
+              <span className="badge badge-sm indicator-item">0</span>
             </div>
           </label>
           <div
             tabIndex={0}
             className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
           >
-            <div className="card-body">
+            <div className="card-body ">
               <span className="font-bold text-lg">8 Items</span>
               <span className="text-info">Subtotal: $999</span>
               <div className="card-actions">
@@ -106,8 +86,8 @@ const Navbar = () => {
         </div>
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <img src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" />
+            <div className="w-10 rounded-full ">
+              <img src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp " />
             </div>
           </label>
           <ul
@@ -130,6 +110,7 @@ const Navbar = () => {
         </div>
       </div>
     </div>
+    
   );
 };
 
