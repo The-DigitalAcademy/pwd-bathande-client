76b46c9518d1b39d08ddf803dcaab4a09360be06
Darklove12 <bsinukela31@gmail.com>
2023-08-29 08:38:27 +0200

    added a new picture

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Header from './components/navbar/Header'
+import Banner from './components/banner/Banner'
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <Banner/>
     </div>
 
   )           
