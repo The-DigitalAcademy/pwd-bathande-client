bd28a860a186bf027b4bf294725c30ad65bcf519
Mbuyiselo91 <mbuyisolo@gmail.com>
2023-08-29 11:13:20 +0200

    ADDED file new contextState

--- client/context/contextState.jsx
+++ client/context/contextState.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
+import React from "react";
 import MyContext from './MyContext'
 
-function ContextState({children}) {
-
-  const data = [
-    {id: 1, username: 'bathande@gmail.com', Email: '1234'}
-  ]
-
-  return (
-    <MyContext.Provider value={data}>
-         {children}
-    </MyContext.Provider>
-  )
+function ContextState({children}){
+    const data = [
+        {id: 1, usename: 'bathande@gmail.com',Email:'1234'}
+    ]
+    return (
+        <MyContext.Provider value={data}>
+            {children}
+        </MyContext.Provider>
+    )
 }
-
-export default ContextState
