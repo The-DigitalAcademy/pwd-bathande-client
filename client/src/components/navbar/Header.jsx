73d95dcd4aeb24408999e1b43ab6ebf401c2620f
Tshepo2022 <tshepotebe@gmail.com>
2023-08-29 14:33:17 +0200

    Header fixed

--- client/src/components/navbar/Header.jsx
+++ client/src/components/navbar/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 const Header = () => {
   return (
@@ -7,104 +7,45 @@ const Header = () => {
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Home</a>
-              </li>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <li><a>Home</a></li>
               <li>
                 <a>Parent</a>
                 <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
+                  <li><a>Submenu 1</a></li>
+                  <li><a>Submenu 2</a></li>
                 </ul>
               </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              <li><a>Item 3</a></li>
             </ul>
           </div>
           <a className="btn btn-ghost normal-case text-xl"></a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Home
-              </a>
+            <li><a>Home</a></li>
+            <li><a>New Arrivals</a></li>
+            <li tabIndex={0}>
+              <details>
+                <summary>Clothes</summary>
+                <ul className="p-2">
+                  <li><a>Men</a></li>
+                  <li><a>Women</a></li>
+                </ul>
+              </details>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/NewArrivals">
-                NewArrivals
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/Men">
-                Men
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/Women">
-                Women
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/Promos">
-                Promos
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/Gift Card">
-                Gift Card
-              </a>
-            </li>
+            <li><a>Promos</a></li>
+            <li><a>Gift Card</a></li>
+
+            <li><a>Sign Up</a></li>
           </ul>
         </div>
+
       </div>
     </div>
+  )
+}
 
-    <a className="btn btn-ghost normal-case text-xl"></a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Home</a></li>
-      <li><a>New Arrivals</a></li>
-      <li tabIndex={0}>
-        <details>
-          <summary>Clothes</summary>
-          <ul className="p-2">
-            <li><a>Men</a></li>
-            <li><a>Women</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Promos</a></li>
-      <li><a to=''>Gift Card</a></li>
-
-
-  );
-};
-
-
-export default Header;
+export default Header
