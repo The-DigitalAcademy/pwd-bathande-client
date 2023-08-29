50637769c10fb2f4f2eacded58de869b3ea225e3
Tshepo2022 <tshepotebe@gmail.com>
2023-08-29 13:14:16 +0200

    giftcard added

---
+++ client/src/components/Giftcards/Giftcard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Giftcard = () => {
+  return (
+    <div>
+
+        <div className='Content'>
+            <div className='Content-wrapper'>
+                <img src="https://purpulhair.co.za/wp-content/uploads/2021/02/gift-card.jpg" alt="" className='zoomCard'/>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Giftcard
