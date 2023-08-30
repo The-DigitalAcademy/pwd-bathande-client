0e8a250ae822202e69f11c1ba4cf504f832bcd87
Darklove12 <bsinukela31@gmail.com>
2023-08-30 13:36:54 +0200

    Added import Data

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-// import { data} from "../data";
+import { data} from "../data";
 // import {Link} from 'react-router-dom'
 
 // const Category = () => {
