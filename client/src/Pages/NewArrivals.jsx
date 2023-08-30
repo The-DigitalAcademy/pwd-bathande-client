4dae7b20991a1f840a23cf269f7a2dcee115062f
Tshembani30 <thembaniasasent982@gmail.com>
2023-08-30 14:23:20 +0200

    added map ,card-title

--- client/src/Pages/NewArrivals.jsx
+++ client/src/Pages/NewArrivals.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
-import Navbar from '../components/navbar/Navbar'
-import Header from '../components/navbar/Header'
+import React from "react";
+import { Link } from "react-router-dom"
+import Navbar from "../components/navbar/Navbar";
+import Header from "../components/navbar/Header";
+import ProductNewArrivals from "../Data/ProductNewArrivals";
 
 function NewArrivals() {
   return (
     <div>
-       <Navbar/>
-        <Header/>
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <Navbar />
+      <Header />
+
+      {ProductNewArrivals.map((card,(index) => (
+          <div key={index} className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img src={Card.Image} className="" alt="..." />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{card.name}</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <Link to={`/list/${card.name}`}>
+                <button className="btn btn-primary">Buy Now</button>
+                </Link>
+                
+              </div>
+            </div>
+          </div>
+        ))
+      )}
     </div>
-  </div>
-</div>
-    </div>
-  )
+  );
 }
 
-export default NewArrivals
+export default NewArrivals;
