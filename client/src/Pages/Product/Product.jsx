30b36898c329e0f1a0dfa39e66d80546bbb3e7f1
Darklove12 <bsinukela31@gmail.com>
2023-08-30 14:55:47 +0200

    added image and product title

--- client/src/Pages/Product/Product.jsx
+++ client/src/Pages/Product/Product.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Product = ({product}) => {
   return (
     <div className='product'>
-        
+        <img src={product?.thumbnail} />
+         {product.title}
+         <br />
+         {product.category}
     </div>
   )
 }
