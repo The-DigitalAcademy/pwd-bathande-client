ff61f242b5202c3744305cb5c7a63bd57980fc4f
Darklove12 <bsinukela31@gmail.com>
2023-08-30 14:21:22 +0200

    added Product to import

--- client/src/components/CategoryProducts/CategoryProducts.jsx
+++ client/src/components/CategoryProducts/CategoryProducts.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {data} from "../data";
-// import Product from '../components/Product'
+import Product from '../components/Product'
 
 // const CategoryProducts = () => {
 //   const { name } = useParams();
