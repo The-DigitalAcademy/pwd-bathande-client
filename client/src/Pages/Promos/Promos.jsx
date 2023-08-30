e9302c71fb68eb395717e3522f01878df6123845
unknown <you@example.com>
2023-08-30 12:54:21 +0200

    add separate content

--- client/src/Pages/Promos/Promos.jsx
+++ client/src/Pages/Promos/Promos.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import '../Promos/Promo.css'
-import Navbar from '../../components/navbar/Navbar'
-import Header from '../../components/navbar/Header'
+import React from "react";
+import "../Promos/Promo.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/navbar/Header";
 const Promos = () => {
   return (
     <div>
-        <Navbar/>
-        <Header/>
+      <Navbar />
+      <Header />
+      <div className="flex flex-col w-full lg:flex-row">
+        <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
+          Female
+        </div>
+        <div className="divider lg:divider-horizontal">OR</div>
+        <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
+          Male
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Promos
+export default Promos;
