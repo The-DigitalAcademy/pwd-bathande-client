66e98119819b57683f65d252a212b2d707e07664
Darklove12 <bsinukela31@gmail.com>
2023-08-31 14:26:39 +0200

    added everything on front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/navbar/Header'
 import Banner from './components/banner/Banner'
 import Footer from './components/Footer/Footer'
 import Category from './components/Category/Category'
+import Video from './components/Video/Video'
 
 const App = () => {
   return (
@@ -11,9 +12,9 @@ const App = () => {
       <Navbar/>
       <Header/>
       <Banner/>
+      <Video/>
       <Category/>
       <Footer/>
-     
     </div>
 
   )           
--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -1,37 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { data } from "../../Data/data";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Category = () => {
-
-
   const [categoryNames, setCategoryNames] = useState([]);
-  
+
   useEffect(() => {
     setUniqueCategory();
   }, []);
 
-
   // get unique category
   function setUniqueCategory() {
-
     // array that going to holds the categories
     let categoryData = [];
 
     // loop through all the data
     for (let i = 0; i < data.length; i++) {
-
       // if categery name is not already in the categoryData, add it
       if (!categoryData.includes(data[i].category)) {
-         categoryData.push(data[i].category);
+        categoryData.push(data[i].category);
       }
-       
     }
 
     // add all the usique category name in the categoryNames state
     setCategoryNames(categoryData);
   }
-
 
   return (
     <div className="category">
